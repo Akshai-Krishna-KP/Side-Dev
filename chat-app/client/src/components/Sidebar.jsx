1d143a1953a546d5ed573872c96f9b4758cf7c9e
Akshai Krishna KP <akshaikrishna42@gmail.com>
2026-01-28 01:04:27 +0530

    Reviewed Sidebar

--- chat-app/client/src/components/Sidebar.jsx
+++ chat-app/client/src/components/Sidebar.jsx
@@ -1,28 +1,50 @@
+// This is Client Side Sidebar Component
+// Initiale thought of this to show the people that are active and in the network
+// In App.jsx they will call a HTTP request every few second to check and rerender this component
+// Maybe should have changed to useState instead of useConst
+
+// This take three argument
+// users : List -> this contain all the userslist
+// activeUser : User -> single active user that reader has finally selected
+// onUserSelect : Function -> change the activeUser in the App.jsx subsequentally affect the activeuser argumnet
 function Sidebar({ users, activeUser, onUserSelect }) {
-    return (
-        <aside className="border-2 border-black-200">
-            <div className="p-4 border-b border-black-200">
-                <h1 className="text-2xl font-bold">Samsaram</h1>
-                <p className="text-sm text-gray-500">Conversations</p>
+  return (
+    // uses aside tag for sidebar
+    <aside className="border-2 border-black-200">
+      {/* // Side bar header can be used to name the chat-app */}
+      <div id="Side-bar-header" className="p-4 border-b border-black-200">
+        <h1 className="text-2xl font-bold">Samsaram</h1>
+        <p className="text-sm text-gray-500">Conversations</p>
+      </div>
+      {/* // Show the content of the users List */}
+      <div className="border-2 border-red-200">
+        {/* // work similar to for-each loop
+        // if users is empty return a [] and thus no rendering */}
+        {users.map((user) => {
+          // used to highlight the activeUser
+          const isActive = activeUser && activeUser.id === user.id;
+          return (
+            // every react component apparently need a unique ID for some reason :(
+            <div key={user.id} onClick={() => onUserSelect(user)}>
+              <div>
+                <div className={isActive ? "font-semibold bg-[#6f554a]" : ""}>
+                  {user.name}
+                </div>
+              </div>
             </div>
+          );
+        })}
 
-            <div className="border-2 border-red-200">
-                {users.map((user) => (
-                    <div onClick={() => onUserSelect(user)}>
-                        <div className="user-info">
-                            <div className="user-name">{user.name}</div>
-                        </div>
-                    </div>
-                ))}
-
-                {users.length === 0 && (
-                    <div className="p-4 text-center text-gray-500">
-                        <p>No active users</p>
-                    </div>
-                )}
-            </div>
-        </aside>
-    );
+        {/* If the above become false, this will be true
+          therefore rener that No Active User */}
+        {users.length === 0 && (
+          <div className="p-4 text-center text-gray-500">
+            <p>No active users</p>
+          </div>
+        )}
+      </div>
+    </aside>
+  );
 }
 
 export default Sidebar;
